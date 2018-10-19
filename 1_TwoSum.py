43944e4aa3c3e1a0bf29fc18168b71fa28c79a8a
godestW <godestwang@gmail.com>
2018-10-19 21:01:32 +0800

    add 1. TwoSum

---
+++ 1_TwoSum.py
@@ -0,0 +1,27 @@
+# Simple
+for i in range(len(nums)):
+    left = nums[i+1:]
+    for j in range(len(left)):
+        if (nums[i] + left[j]) == target:
+            return i, j+i+1
+
+
+# Two-pass Hash Table
+hash_table={}
+for i in range(len(nums)):    # make a hash table first
+    hash_table[nums[i]]=i
+for i in range(len(nums)):
+    if target-nums[i] in hash_table:
+        if hash_table[target-nums[i]] != i:
+            return [i, hash_table[target-nums[i]]]
+return []
+
+
+# One-pass Hash Table
+hash_table = {}
+for i, num in enumerate(nums):
+    if target - num in hash_table:
+        return([hash_table[target - num], i])
+        break    # someone add break here, which make Runtime faster!!
+    hash_table[num] = i
+return([])
---
+++ README.md
@@ -0,0 +1,5 @@
+## LeetCode Sample Code (Python)
+
+1.  [TwoSum](https://medium.com/@havbgbg68/leetcode-1-two-sum-python-8d77c223abd3) 
+2.  [Add to Numbers](https://medium.com/@havbgbg68/leetcode-1-two-sum-python-8d77c223abd3) 
+
