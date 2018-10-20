aac661169824057cd60efbc87b751d207d3776fa
godestW <godestwang@gmail.com>
2018-10-20 15:25:09 +0800

    add 2. Add Two Numbers

---
+++ 2_AddTwoNumbers.py
@@ -0,0 +1,24 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        node = result
+        temp = 0
+        while l1 is not None or l2 is not None or temp>0:
+            if l1 is not None:
+                temp += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                temp += l2.val
+                l2 = l2.next
+
+            node.next = ListNode(temp%10)
+            node = node.next
+            
+            temp = temp // 10
+
+        return result.next
